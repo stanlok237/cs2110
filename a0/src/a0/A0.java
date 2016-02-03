f4ea92bac2c56e8ee4362fc952a10f5c9406dcdf
Stanley Lok <stanlok237@live.com>
2016-02-03 14:13:29 -0500

    Add A0 and A1

---
+++ a0/src/a0/A0.java
@@ -0,0 +1,14 @@
+package a0;
+
+public class A0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Executing method main.");
+		int x= 5;
+		System.out.println("x is now " + x);
+		assert x == 6;
+		System.out.println("The assert statement was not executed");
+	}
+
+}
---
+++ a1PhD/src/a1PhD/PhD.java
@@ -0,0 +1,8 @@
+package a1PhD;
+
+/** NetId: nnnnn, nnnnn. Time spent: hh hours, mm minutes.
+An instance maintains info about the PhD of a person. */
+
+public class PhD {
+
+}
